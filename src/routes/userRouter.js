1a569c2f67e5e66dad59e72268eed160a0e5119f
christianubb <roblesfontc@gmail.com>
2023-01-10 17:00:40 -0300

    userRouter

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
-const {createUser, getAllUser} = require('../controllers/userController')
+const {createUser, getAllUser, getUser, updateUser, deleteUser} = require('../controllers/userController')
 
 const router = Router();
 
 //Routes
 router.post("/", createUser);
 router.get("/", getAllUser)
+router.get("/:id", getUser)
+router.patch("/:id", updateUser)
+router.delete("/:id", deleteUser)
 
 
 module.exports = router;
