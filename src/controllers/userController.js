46168a6463aec59c12cd6848721969e4ee21830a
Alejandra Besel <alejandrabesel@Alejandras-MacBook-Pro.local>
2023-01-11 12:34:24 -0300

    changes to merge

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,23 +3,60 @@ const {User} = require('../db')
 const createUser = async(req, res) =>{
   try {
     const { name, email } = req.body;
-    console.log(name);
     const newUser = await User.create({
       name,
       email,
     });
-    console.log(newUser);
-    res.json(newUser);
+    res.status(200).json(newUser);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 }
 
 const getAllUser = async(req,res) => {
-  
-} 
+  try{
+    const allUser = await User.findAll()
+    res.status(200).json(allUser)
+  } catch{
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+const getUser = async(req,res) => {
+  try{
+    const {id} = req.params
+    const user = await User.findOne({where: {id:id}})
+    res.status(200).send(user)
+  } catch{
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+const updateUser = async(req,res) => {
+  try{
+    const {id} = req.params
+    const {name, email} = req.body
+    await User.update({name:name, email:email}, {where:{id:id}})
+    res.status(200).send("user update")
+  } catch{
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+const deleteUser = async(req,res) => {
+  try{
+    const {id} = req.params
+    await User.destroy({where: {id:id}})
+    res.status(200).send("user delete")
+  } catch{
+    return res.status(500).json({ message: error.message });
+  }
+}
 
 module.exports = {
     createUser,
-    getAllUser
+    getAllUser,
+    getUser,
+    updateUser,
+    deleteUser
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,24 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
-const userRouter = require('./userRouter');
+
+const userRouter = require("./userRouter");
+const petRouter = require("./petRouter");
+const specieRouter = require("./specieRouter");
+const raceRouter = require("./raceRouter");
 const postRouter = require('./postRouter');
+
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
 
-
 // Configurar los routers
-router.use('/user', userRouter);
+
+router.use("/user", userRouter);
+router.use("/pet", petRouter);
+router.use("/specie", specieRouter);
+router.use("/race", raceRouter);
+router.use("/post", postRouter);
+
 // Ejemplo: router.use('/auth', authRouter);
-router.use('/post', postRouter);
+
 
 module.exports = router;
