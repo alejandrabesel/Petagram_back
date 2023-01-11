653ae81c781e31c807c1000ab7ba9f3ac772fe7a
Alejandra Besel <alejandrabesel@Alejandras-MacBook-Pro.local>
2023-01-11 12:47:59 -0300

    feat/crud_post_address

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,9 @@ const userRouter = require("./userRouter");
 const petRouter = require("./petRouter");
 const specieRouter = require("./specieRouter");
 const raceRouter = require("./raceRouter");
+const postRouter = require('./postRouter');
+const addressRouter = require('./addressRouter');
+
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
 
@@ -12,6 +15,9 @@ router.use("/user", userRouter);
 router.use("/pet", petRouter);
 router.use("/specie", specieRouter);
 router.use("/race", raceRouter);
+router.use("/post", postRouter);
+router.use("/address", addressRouter);
+
 // Ejemplo: router.use('/auth', authRouter);
 
 module.exports = router;
